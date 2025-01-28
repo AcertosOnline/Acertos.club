8575f76d701fe78bcdae61912f3df13c378c4c80
AcertosOnline <acertosonlinejogodobicho@gmail.com>
2025-01-28 19:17:01 -0300

    Create silva-service-worker.js

---
+++ silva-service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'acertos.club.cache';
+const urlsToCache = [
+    '/',
+    '/SilvaJB.html',
+    '/silva-manifest.json',
+    '/icons/icon192.png',
+    '/icons/icon512.png'
+];
+
+// Instala o Service Worker e adiciona os recursos ao cache
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Intercepta as requisições e serve os recursos do cache quando disponíveis
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Retorna o recurso do cache se encontrado
+                if (response) {
+                    return response;
+                }
+                // Caso contrário, busca na rede
+                return fetch(event.request);
+            })
+    );
+});
+
+// Atualiza o Service Worker e remove caches antigos
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
